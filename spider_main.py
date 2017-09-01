4953af14f8b25f0f6023c32d2e78b886b6270303
Maidoudou <lichanglin163@163.com>
2017-09-01 21:51:14 +0800

    [spider baike python]

---
+++ spider_main.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from spider_baidu import url_manager
+from spider_baidu import html_downloader
+from spider_baidu import html_parser
+from spider_baidu import html_outputer
+
+class SpiderMain(object):
+    def __init__(self):
+        self.urls       = url_manager.urlManager()
+        self.downloader = html_downloader.downloader()
+        self.parser     = html_parser.parser()
+        self.outputer   = html_outputer.outputer()
+
+    def craw(self, root_url):
+        self.urls.add_new_url(root_url)
+
+        while self.urls.has_new_url():
+            try:
+                count = 1
+                new_url = self.urls.get_new_url()
+                print("craw %d") % count
+                html_cont = self.downloader.downloader(new_url)
+                new_url, new_data = self.html_parser.parser(new_url, html_cont)
+                self.urls.add_new_urls(new_url)
+                self.outputer.collect_data()
+
+                if count == 1000:
+                    break
+
+                count = count + 1
+            except:
+                print("craw fail!")
+
+        self.outputer.outputer_html()
+
+
+
+if __name__ == "__main__":
+    root_url   = "https://baike.baidu.com/item/Python/407313?fr=aladdin"
+    obj_spider = SpiderMain()
+    obj_spider.craw(root_url)
